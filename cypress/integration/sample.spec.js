d3b712629b9226e1d29a46fd29de09120c306a47
Yurii Grynkov <yugrunkov@gmail.com>
2019-12-28 16:22:03 +0200

    negative scenario check

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -1,6 +1,6 @@
 describe('Home Page', function() {
     it('Check site title', function() {
       cy.visit('/')
-      cy.get('[data-test="siteTitle"]').should('contain', 'Клуб Карника Украина')
+      cy.get('[data-test="siteTitle"]').should('contain', 'Клуб Украина')
     })
   })
