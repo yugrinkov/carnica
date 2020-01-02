ef2769d8c8098306fcf73410a55f62ace3e2f79a
Yurii Grynkov <yugrinkov@gmail.com>
2020-01-02 17:41:53 +0200

    Removed export step from build pipeline

--- next.config.js
+++ next.config.js
@@ -4,11 +4,6 @@ const withImages = require('next-images')
 module.exports = withImages(
   withSass({
     cssModules: true,
-    exportPathMap: function() {
-      return {
-        '/': { page: '/' },
-      }
-    },
     env: {
       CARNICA_PUBLIC_URL: process.env.CARNICA_PUBLIC_URL,
     },
